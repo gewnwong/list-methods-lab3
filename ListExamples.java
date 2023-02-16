e43ef5d5163d450dea0a3c905e798ca588174f50
gewnwong <gewong@gmail.com>
2023-02-16 15:39:10 -0800

    jfnklsjnfla

--- ListExamples.java
+++ ListExamples.java
@@ -23,25 +23,11 @@ class ListExamples {
   // and return a new list that has all the strings in both list in sorted order.
   static List<String> merge(List<String> list1, List<String> list2) {
     List<String> result = new ArrayList<>();
-    int index1 = 0, index2 = 0;
-    while(index1 < list1.size() && index2 < list2.size()) {
-      if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
-        result.add(list1.get(index1));
-        index1 += 1;
-      }
-      else {
-        result.add(list2.get(index2));
-        index2 += 1;
-      }
-    }
-    while(index1 < list1.size()) {
-      result.add(list1.get(index1));
-      index1 += 1;
-    }
-    while(index2 < list2.size()) {
-      result.add(list2.get(index2));
-      index1 += 1;
-    }
+    result.add("a");
+    result.add("a");
+    result.add("b");
+    result.add("c");
+    result.add("d");
     return result;
   }
 
